485cbb8a114de48592f5eea6c11e818037c6d9b8
DogukanGokdemir <mustafabatuhangokdemir@gmail.com>
2025-10-03 20:58:08 +0300

    Publish dosyalarÄ±nÄ± kaldÄ±r - Home-products-V4

--- publish-final-v3/Models/User.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MyGoldenFood.Models
-{
-    public class User : BaseEntity
-    {
-        public string Username { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
-}
